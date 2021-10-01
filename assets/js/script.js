b359dc2e0ebb65b11532e5ca52a3513af840a932
josephdaw <josephdaw@icloud.com>
2021-10-01 13:58:31 +0930

    add function to store project form values and clear the modal card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,31 @@ const projectFormTypeEl = $('#project-type-input');
 const projectFormRateEl = $('#project-rate-input');
 const projectFormDueDateEl = $('#project-due-date-input');
 const projectFormBtn = $('#project-form-btn')
+
+let projects = [];
+
+// eventlistener for submit on modal form
+projectFormEl.on("click", '#project-form-btn', createNewProject);
+
+function createNewProject(){
+    // add new project to projects array
+    projects.push({
+        name: projectFormNameEl.val(),
+        type: $('#project-type-input option:selected').text(),
+        rate: projectFormRateEl.val(),
+        dueDate: projectFormDueDateEl.val()
+    });
+
+    // clear the form
+    clearProjectForm();
+
+    // close the modal window
+    $('#project-form-modal').modal('hide');
+};
+
+function clearProjectForm(){
+    projectFormNameEl.val('');
+    projectFormTypeEl.prop('selectedIndex', 0);
+    projectFormRateEl.val('');
+    projectFormDueDateEl.val('');
+};
